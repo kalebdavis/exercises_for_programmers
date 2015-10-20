835562d5f729d7a74ab8f05f952279d3e7864d27
Kaleb Davis <kalebdavisgithub@gmail.com>
2015-10-19 20:55:26 -0400

    complete first two parts of challenge

--- chap3/convert_currency.c
+++ chap3/convert_currency.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 
 int main() {
+  convert_euros();
+  convert_anything();
+  return 0;
+}
+
+int convert_euros() {
   float amountTo, amountFrom, rateFrom = 0.0;
   float rateTo = 100;
   printf("How many euros are you exchanging?\n");
@@ -16,5 +22,39 @@ int main() {
   }
   amountTo = (amountFrom * rateFrom) / rateTo;
   printf("%.2f euros at an exchange rate of %.2f is\n%.2f U.S. dollars.\n", amountFrom, rateFrom, amountTo);
+  return 0;
+}
+
+int convert_anything() {
+  char *countries[5] = { "britain", "canada", "france", "australia", "utopia" };
+  float rates[5] = { 154.707, 76.79, 113.27, 72.6, 300};
+  char countryFrom[16];
+  float amountTo, amountFrom, rateFrom = 0.0;
+  float rateTo = 100;
+  printf("How many bills are you exchanging?\n");
+  while(scanf("%f", &amountFrom) != 1){
+    printf("Needs to be in format 00.00\n");
+    scanf("%*s");
+  }
+  printf("What country are you from?\n");
+  while(scanf("%s", &countryFrom) != 1){
+    printf("Needs to be a string\n");
+    scanf("%*s");
+  }
+  int index;
+  printf("Country from: %s\n", countryFrom);
+  for(index = 0; index < 5; index++){
+    if(strcmp(countries[index],  countryFrom) == 0){
+      rateFrom = rates[index];
+      break;
+    }
+  }
+  if(rateFrom == 0.0){
+    printf("We don't have your country on file, sorry\n");
+  }
+  else{
+    amountTo = (amountFrom * rateFrom) / rateTo;
+    printf("%.2f euros at an exchange rate of %.2f is\n%.2f U.S. dollars.\n", amountFrom, rateFrom, amountTo);
+  }
   return 0;
 }
